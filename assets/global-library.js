9042ee7dcfffdc1050eca5bfd293d077c062e728
Wyatt Chamberlin <wyattchamberlin6@gmail.com>
2025-06-30 14:26:33 -0400

    adding license to vin option to garage, fixed vehicle spelling error in functins

--- assets/global-library.js
+++ assets/global-library.js
@@ -88,6 +88,17 @@ const handleVinChange = (event, functionLocation, errorMsg) => {
   }
 };
 
+const handleLicenseChange = (event, functionLocation, errorMsg) => {
+  let licenseInput = event.target.value.toUpperCase();
+  event.target.value = licenseInput;
+  const licenseBtn = document.getElementById('license-to-vin-btn');
+  if (licenseInput.length > 0) {
+    licenseBtn.disabled = false;
+  } else {
+    licenseBtn.disabled = true;
+  }
+}
+
 async function createCompanyProfile(companyName, firstName, lastName, email, phone, title, address, city, zipCode, stateAbbr, locationName, pdf, taxRegistrationId, typeText, reasonText) {
   try {
     const response = await fetch('https://tax-exemption-signup-505215902673.us-east5.run.app/createCustomerProfile', {
@@ -126,12 +137,14 @@ async function createCompanyProfile(companyName, firstName, lastName, email, pho
     functionLocation - 1 = Garage
     functionLocation - 2 = Vin Verification
 */
-async function fetchVehicalDataByVin(vin, functionLocation, noResults, failed3times, searchBtn, tailoredSuccessMessage, remainingAttempts) {
+async function fetchVehicleDataByVin(vin, functionLocation, noResults, failed3times, searchBtn, tailoredSuccessMessage, remainingAttempts) {
   const vinSearchBtn = document.getElementById("vin-to-collection-btn");
   const errorMessage = document.querySelector('.errorMessage');
   const maxAttempts = 3;
   const calculatedRemainingAttempts = maxAttempts - attemptedVinsVinVerification.length;
   const vinGuaranteeFetchingMsg = document.querySelector('.vin-verification-fetching-vin-data');
+
+  console.log('fetchVehicleDataByVin::vin: ', vin);
 
   try {
     if (vinGuaranteeFetchingMsg) vinGuaranteeFetchingMsg.style.display = "block";
@@ -147,7 +160,7 @@ async function fetchVehicalDataByVin(vin, functionLocation, noResults, failed3ti
       throw new Error(`Error fetching order details. Status: ${response.status}. Message: ${response.statusText}.`);
     }
     const data = await response.json();
-    console.log('data.handle: ', data);
+    console.log('fetchVehicleDataByVin::data: ', data);
 
     if (functionLocation === 1) {
       // NOTE: The data returns the paint code here! No need to alter gcr function as I can 
@@ -240,5 +253,119 @@ async function fetchVehicalDataByVin(vin, functionLocation, noResults, failed3ti
       vinSearchBtn.classList.remove("vin-to-collection-btn--loading");
       vinSearchBtn.innerHTML = searchBtn;
     }
+  }
+}
+
+async function fetchBumpersLicenseToVin(state, plate) {
+  try {
+    console.log('Fetching vehicle data by license plate...');
+    const response = await fetch(`https://license-to-vin-273472976974.us-east5.run.app/license-to-vin?state=${state}&plate=${plate}`, {
+      method: 'Get',
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error fetching order details. Status: ${response.status}. Message: ${response.statusText}.`);
+    }
+    const data = await response.json();
+    console.log('data.handle: ', data);
+    const vin = data.entities?.vehicles?.automobiles[0]?.vin
+    const year = data.entities?.vehicles?.automobiles[0]?.year;
+    const make = data.entities?.vehicles?.automobiles[0]?.make;
+    const model = data.entities?.vehicles?.automobiles[0]?.model;
+
+    return { vin, year, make, model };
+
+    // if (functionLocation === 1) {
+    //   // NOTE: The data returns the paint code here! No need to alter gcr function as I can 
+    //   // just use the paint code from the data object here.
+    //   if (data.handle === '') {
+    //     if (attemptedVinsInGarage.length < maxAttempts) {
+    //       document.querySelector('.errorMessage').innerHTML = noResults;
+    //     } else {
+    //       document.querySelector('.errorMessage').innerHTML = failed3times;
+    //       document.getElementById('vin').disabled = true;
+    //       return;
+    //     }
+    //     garageVinSubmissionBool = false;
+    //     return;
+    //   } else {
+    //     // Don't add garageVinSubmissionBool here because there will be a time when the button
+    //     // is clickable between submission and page navigation.  Don't wan't anyone spamming
+    //     // the button.
+    //     updateGarageAndNavigate(data.handle, vin);
+    //   }
+    // } else if (functionLocation === 2) {
+    //   if (data.handle === '' || data.isVinValid === false) {
+    //     if (attemptedVinsVinVerification.length < maxAttempts) {
+
+    //       document.querySelector('.vin-verification-submission-failed-message').innerHTML = noResults;
+    //       document.querySelector('.vin-verification-remaining-attempts').style.display = "block";
+    //       document.querySelector('.vin-verification-remaining-attempts').innerHTML = remainingAttempts + calculatedRemainingAttempts;
+    //     } else {
+    //       document.querySelector('.vin-verification-submission-failed-message').innerHTML = failed3times;
+    //       // document.getElementById('vin-textbox-for-verification').value = '';
+    //       // document.getElementById('vin-textbox-for-verification').disabled = true;
+    //       document.querySelector('.vin-verification-remaining-attempts').style.display = "block";
+    //       document.querySelector('.vin-verification-remaining-attempts').innerHTML = remainingAttempts + calculatedRemainingAttempts;
+    //       finalVinVerificationSubmissionVin = vin;
+    //     }
+    //     document.querySelector('.vin-verification-submission-success-message').style.display = "none";
+    //     document.querySelector('.vin-verification-submission-failed-message').style.display = "block";
+    //     return false;
+    //   } else {
+    //     document.querySelector('.vin-verification-submission-success-message').innerHTML = tailoredSuccessMessage + `${data.year} ${data.make} ${data.model}`;
+    //     document.querySelector('.vin-verification-submission-failed-message').style.display = "none";
+    //     document.querySelector('.vin-verification-submission-success-message').style.display = "block";
+    //     document.getElementById('vin-textbox-for-verification').disabled = true;
+    //     document.querySelector('.vin-verification-remaining-attempts').style.display = "none";
+
+    //     finalVinVerificationSubmissionVin = vin;
+    //     successfulVinVerificationVin = true;
+
+    //     return true;
+    //   }
+    // }
+  } catch (error) {
+    // console.log('attemptedVinsVinVerification: ', attemptedVinsVinVerification);
+    // if (functionLocation === 1) {
+    //   if (errorMessage.style.visibility === 'hidden') {
+    //     errorMessage.style.visibility = 'visible';
+    //   }
+    //   vinSearchBtn.disabled = true;
+    //   if (attemptedVinsInGarage.length < maxAttempts) {
+    //     document.querySelector('.errorMessage').innerHTML = noResults;
+    //   } else {
+    //     document.querySelector('.errorMessage').innerHTML = failed3times;
+    //     document.getElementById('vin').disabled = true;
+    //     console.error("Error fetching vehicle data:", error);
+    //     return;
+    //   }
+    //   garageVinSubmissionBool = false;
+    // } else if (functionLocation === 2) {
+    //   if (attemptedVinsVinVerification.length < maxAttempts) {
+    //     document.querySelector('.vin-verification-submission-failed-message').innerHTML = noResults;
+    //     document.querySelector('.vin-verification-remaining-attempts').style.display = "block";
+    //     document.querySelector('.vin-verification-remaining-attempts').innerHTML = remainingAttempts + calculatedRemainingAttempts;
+    //   } else {
+    //     document.querySelector('.vin-verification-submission-failed-message').innerHTML = failed3times;
+    //     document.getElementById('vin-textbox-for-verification').disabled = true;
+    //     document.querySelector('.vin-verification-remaining-attempts').style.display = "block";
+    //     document.querySelector('.vin-verification-remaining-attempts').innerHTML = remainingAttempts + calculatedRemainingAttempts;
+    //     finalVinVerificationSubmissionVin = vin;
+    //   }
+    //   document.querySelector('.vin-verification-submission-success-message').style.display = "none";
+    //   document.querySelector('.vin-verification-submission-failed-message').style.display = "block";
+    //   console.error("Error fetching vehicle data:", error);
+    //   return false;
+    // }
+    console.error("Error fetching vehicle data:", error);
+  } finally {
+    // if (vinGuaranteeFetchingMsg) vinGuaranteeFetchingMsg.style.display = "none";
+
+    // if (functionLocation === 1) {
+    //   vinSearchBtn.classList.remove("vin-to-collection-btn--loading");
+    //   vinSearchBtn.innerHTML = searchBtn;
+    // }
   }
 }
